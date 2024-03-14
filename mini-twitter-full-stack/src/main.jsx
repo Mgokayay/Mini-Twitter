3d689b9f7057ec3a22feae515003cd5f91d01eed
Mgokayay <aymustafagokay@gmail.com>
2024-03-14 23:14:50 +0300

    route routes provider given

--- mini-twitter-full-stack/src/main.jsx
+++ mini-twitter-full-stack/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // BrowserRouter ekledik
+import { store } from "./store/store.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+    </Router>
+  </Provider>
+);
