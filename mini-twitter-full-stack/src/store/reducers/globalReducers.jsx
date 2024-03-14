6e99b74718983238113502634e991c235ff907d2
Mgokayay <aymustafagokay@gmail.com>
2024-03-14 23:16:56 +0300

    globalreducers created

---
+++ mini-twitter-full-stack/src/store/reducers/globalReducers.jsx
@@ -0,0 +1,7 @@
+const initialValues = { state: { loggedIn: false } };
+export const globalReducer = (state = initialValues, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
